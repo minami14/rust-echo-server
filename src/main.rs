272fb2bf9521de5b33c8fa3a67fa9ff2c3ec0857
minami14 <minami-git@minami-vr.sakura.ne.jp>
2019-11-27 19:59:06 +0900

    Initail commit

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+use std::io::{Error, Read, Write};
+use std::net::{TcpListener, TcpStream};
+use std::thread;
+
+fn main() {
+    let listener = TcpListener::bind("0.0.0.0:1234").expect("failed to bind");
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                thread::spawn(move || {
+                    echo(stream).unwrap_or_else(|err| eprintln!("{}", err))
+                });
+            }
+            Err(err) => eprintln!("{}", err)
+        }
+    }
+}
+
+fn echo(mut stream: TcpStream) -> Result<(), Error> {
+    let mut buf = [0u8; 1024];
+    match stream.read(&mut buf) {
+        Ok(n) => {
+            match stream.write(&buf[..n]) {
+                Ok(_) => stream.flush(),
+                Err(err) => Err(err)
+            }
+        }
+        Err(err) => Err(err)
+    }
+}
